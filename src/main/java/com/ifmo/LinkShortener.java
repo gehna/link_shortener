1bb6a1c55f1d0f7f028abd159aa7902bf73cb074
OlgaLsh <73122567+OlgaLsh@users.noreply.github.com>
2021-12-03 20:55:53 +0300

    --class LinkShortener

---
+++ src/main/java/com/ifmo/LinkShortener.java
@@ -0,0 +1,12 @@
+package com.ifmo;
+
+import java.util.HashMap;
+
+public class LinkShortener implements ShortenerActions {
+    HashMap<String, String> ListOfLinks = new HashMap<>();
+
+    public void remove() {
+
+
+    }
+}
