0bfe4f2e16c51e2735ca0cd1637c3975d33d27b4
Gennadii Legkodukhov <gehna@yandex.ru>
2021-12-03 21:13:12 +0300

    -implementing add method

--- src/main/java/com/ifmo/LinkShortener.java
+++ src/main/java/com/ifmo/LinkShortener.java
@@ -1,12 +1,34 @@
 package com.ifmo;
 
 import java.util.HashMap;
+import java.util.Random;
 
 public class LinkShortener implements ShortenerActions {
     HashMap<String, String> ListOfLinks = new HashMap<>();
 
-    public void remove() {
+    @Override
+    public void add(String link) {
+        if (ListOfLinks.containsValue(link)){
+            System.out.println("Value "+ link + " is already exist");
+        } else {
+            String shortLink;
+            String schema = link.substring(0, link.indexOf("://"));
+            String domain = ".lk.ru";
+
+            int leftLimit = 97; // letter 'a'
+            int rightLimit = 122; // letter 'z'
+            int targetStringLength = 5;
+            Random random = new Random();
+            StringBuilder buffer = new StringBuilder(targetStringLength);
+            for (int i = 0; i < targetStringLength; i++) {
+                int randomLimitedInt = leftLimit + (int)
+                        (random.nextFloat() * (rightLimit - leftLimit + 1));
+                buffer.append((char) randomLimitedInt);
+            }
+            shortLink = buffer.toString();
 
 
+            ListOfLinks.put(convert(link), link);
+        }
     }
 }
--- src/main/java/com/ifmo/ShortenerActions.java
+++ src/main/java/com/ifmo/ShortenerActions.java
@@ -1,7 +1,7 @@
 package com.ifmo;
 
 public interface ShortenerActions {
-    public void add();
-    public void remove();
-    public void convert();
+    public void add(String link);
+    public void remove(String link);
+    public void convert(String link);
 }
